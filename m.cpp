330a66f6d1d2eb846046d5047f453ab4438d4df4
Mohamed <bbb18381@gmail.com>
2023-12-24 21:09:49 +0200

    sum4 main

--- m.cpp
+++ m.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
-int sum2(int a, int b);
+int sum4(int a, int b);
 int main() {
 	cout << "ayman " << endl;
 	cout << "mohamed " << endl;
 	int z;
-	z = sum2(3, 4);
+	z = sum4(3, 4);
 	cout << z << endl;
 	return 0;
 
 }
 
-sum2(int a, int b) {
+sum4(int a, int b) {
 	int x;
 	x = a + b;
 	return x;
